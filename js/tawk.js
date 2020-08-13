e345d4848c371be1fc3a8901f60176d1f9aeabcc
urienix <urienix@hotmail.com>
2020-08-12 21:48:06 -0600

    adding google analytics and tawk to have stadistic data and live chat

---
+++ js/tawk.js
@@ -0,0 +1,13 @@
+/* To add tawk to your page only add this script into your html page*/
+
+var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5f34a2924c7806354da5f5e3/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
